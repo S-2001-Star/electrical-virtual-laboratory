60b6efc495bc45187ad2fc1e45149e9e4886f184
Suprita Panda <supritapanda17@gmail.com>
2022-12-17 22:24:51 +0530

    main

--- js/script.js
+++ js/script.js
@@ -1,35 +1,30 @@
-// Get the button that opens the modal
-var btn = document.querySelectorAll(".boat");
-
-// All page modals
-var modals = document.querySelectorAll('.modal');
-
-// Get the <span> element that closes the modal
-var spans = document.getElementsByClassName("close");
-
-// When the user clicks the button, open the modal
-for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
- }
+function scrollWaypointInit( items, trigger ) {
+  items.each( function() {
+    var element = $(this),
+        osAnimationClass = element.data("animation"),
+        osAnimationDelay = element.attr('data-animation-delay');
+ 
+    element.css({
+        '-webkit-animation-delay':  osAnimationDelay,
+        '-moz-animation-delay':     osAnimationDelay,
+        'animation-delay':          osAnimationDelay
+    });
+ 
+    var trigger = ( trigger ) ? trigger : element;
+ 
+    trigger.waypoint(function() {
+        element.addClass('animated').addClass(osAnimationClass);
+    },{
+        triggerOnce: true,
+        offset: '80%'
+    });
+  });
 }
 
-// When the user clicks on <span> (x), close the modal
-for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-    }
- }
-}
+//Call the init
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-     }
-    }
-}
+$(document).ready(function(){
+
+scrollWaypointInit( $('.animateMe') );
+
+});
