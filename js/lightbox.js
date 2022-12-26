0e796aa8dc3276900ec0bcfb8c6f9203a780603f
Suprita Panda <supritapanda17@gmail.com>
2022-12-27 02:21:08 +0530

    main

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,35 +1,48 @@
-// Get the button that opens the modal
-var btn = document.querySelectorAll(".nav-link");
+// // Get the button that opens the modal
+// var btn = document.querySelectorAll(".nav-link");
 
-// All page modals
-var modals = document.querySelectorAll('.modal');
+// // All page modals
+// var modals = document.querySelectorAll('.modal');
 
-// Get the <span> element that closes the modal
-var spans = document.getElementsByClassName("close");
+// // Get the <span> element that closes the modal
+// var spans = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal
-for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
-    e.preventDefault();
-    modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
- }
-}
+// // When the user clicks the button, open the modal
+// for (var i = 0; i < btn.length; i++) {
+//  btn[i].onclick = function(e) {
+//     e.preventDefault();
+//     modal = document.querySelector(e.target.getAttribute("href"));
+//     modal.style.display = "block";
+//  }
+// }
 
-// When the user clicks on <span> (x), close the modal
-for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
-    for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-    }
- }
-}
+// // When the user clicks on <span> (x), close the modal
+// for (var i = 0; i < spans.length; i++) {
+//  spans[i].onclick = function() {
+//     for (var index in modals) {
+//       if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+//     }
+//  }
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-     }
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target.classList.contains('modal')) {
+//      for (var index in modals) {
+//       if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+//      }
+//     }
+// }
+
+
+$(document).on("click","#cust_btn",function(){
+  
+   $("#myModal").modal("toggle");
+   
+ })
+
+ $(document).on("click","#cust_btn1",function(){
+  
+   $("#myModal1").modal("toggle");
+   
+ })
