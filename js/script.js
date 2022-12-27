8a89a27c487a8a2a8bed7ec6e5cfa37b77285685
Suprita Panda <supritapanda17@gmail.com>
2022-12-27 16:50:02 +0530

    main

--- js/script.js
+++ js/script.js
@@ -28,3 +28,20 @@ $(document).ready(function(){
 scrollWaypointInit( $('.animateMe') );
 
 });
+
+
+
+
+//google sign in
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+//reCAPTCHA
+var onloadCallback = function() {
+  alert("grecaptcha is ready!");
+};
