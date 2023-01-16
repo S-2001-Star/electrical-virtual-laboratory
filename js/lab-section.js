78ee137542db173264680c1e9c0608578a6639a0
Suprita Panda <supritapanda17@gmail.com>
2023-01-16 12:04:29 +0530

    main

---
+++ js/lab-section.js
@@ -0,0 +1,14 @@
+function intro_body() {
+    document.getElementById("para").innerHTML = "ojiuhygtfrdeswedrtfgyhu";
+}
+
+
+function object_body() {
+    document.getElementById("para").innerHTML = "szdfhtyj";
+}
+
+
+
+function list_body() {
+    document.getElementById("para").innerHTML = "asdftyj";
+}
