5d731c316f772acf12c5123f23669509fb681c87
Suprita Panda <supritapanda17@gmail.com>
2023-01-23 13:11:28 +0530

    main

--- js/lab-section.js
+++ js/lab-section.js
@@ -13,3 +13,104 @@ function list_body(){
     document.getElementById("para1").style.display = "none";
     document.getElementById("para2").style.display = "block";
 }
+
+//THEVENIN THEOREM
+function exp_intro_body(){
+    document.getElementById("diagram").style.display = "block";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_ob_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "block";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_app_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "block";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_pre_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "block";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_theory_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "block";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_pr_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "block";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_sim_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "block";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_post_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "block";
+    document.getElementById("diagram8").style.display = "none";
+}
+function exp_vd_body(){
+    document.getElementById("diagram").style.display = "none";
+    document.getElementById("diagram1").style.display = "none";
+    document.getElementById("diagram2").style.display = "none";
+    document.getElementById("diagram3").style.display = "none";
+    document.getElementById("diagram4").style.display = "none";
+    document.getElementById("diagram5").style.display = "none";
+    document.getElementById("diagram6").style.display = "none";
+    document.getElementById("diagram7").style.display = "none";
+    document.getElementById("diagram8").style.display = "block";
+}
