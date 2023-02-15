c3beabf8a1cdbc1c5a348775387c8e092efd505b
Suprita Panda <supritapanda17@gmail.com>
2023-02-16 00:04:22 +0530

    main

---
+++ assets/EXPT-4/script.js
@@ -0,0 +1,22 @@
+function thpostlabsubmit(){
+    document.getElementById("thans1").style.display = "block";
+    document.getElementById("thans1").style.color = "green";
+
+
+    document.getElementById("thans2").style.display = "block";
+    document.getElementById("thans2").style.color = "green";
+
+
+    document.getElementById("thans3").style.display = "block";
+    document.getElementById("thans3").innerHTML = "Ans: The depended sources are left intact because they are controlled by other circuit variable."
+    document.getElementById("thans3").style.color = "green";
+
+
+    document.getElementById("thans4").style.display = "block";
+    document.getElementById("thans4").style.color = "green";
+
+
+    document.getElementById("thans5").style.display = "block";
+    document.getElementById("thans5").innerHTML = "Ans: By using the technique we can avoid the problem of analysing the circuit again and again, if the load is varying. "
+    document.getElementById("thans5").style.color = "green";
+}
