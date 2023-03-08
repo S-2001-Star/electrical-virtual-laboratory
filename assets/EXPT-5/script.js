45192dbc86d5f2f87067fdc75bbbede18c418ebf
Suprita Panda <supritapanda17@gmail.com>
2023-03-08 22:25:07 +0530

    main

---
+++ assets/EXPT-5/script.js
@@ -0,0 +1,146 @@
+function nopostlabsubmit(){
+
+//////////ques-1///////////
+
+    if(document.getElementById("noqs1_op1").checked == true){
+        document.getElementById("noqs1_op1").style.display = "block";
+        document.getElementById("noans1").innerHTML = "Ans: To simplify complex circuits  "
+        document.getElementById("noans1").style.color = "green";
+
+    }
+    if(document.getElementById("noqs1_op2").checked == true){
+        document.getElementById("noqs1_op2").style.display = "block";
+        document.getElementById("noans1").innerHTML = "Ans: To simplify complex circuits  "
+        document.getElementById("noans1").style.color = "red";
+
+    }
+    if(document.getElementById("noqs1_op3").checked == true){
+        document.getElementById("noqs1_op3").style.display = "block";
+        document.getElementById("noans1").innerHTML = "Ans: To simplify complex circuits  "
+        document.getElementById("noans1").style.color = "red";
+
+    }
+    if(document.getElementById("noqs1_op4").checked == true){
+        document.getElementById("noqs1_op4").style.display = "block";
+        document.getElementById("noans1").innerHTML = "Ans: To simplify complex circuits  "
+        document.getElementById("noans1").style.color = "red";
+
+    }
+
+//////////ques-2//////////////////
+
+    if(document.getElementById("noqs2_op1").checked == true){
+        document.getElementById("noqs2_op1").style.display = "block";
+        document.getElementById("noans2").innerHTML = "Ans: Norton's theorem is a simplified version of Thevenin's theorem  "
+        document.getElementById("noans2").style.color = "red";
+
+    }
+    if(document.getElementById("noqs2_op2").checked == true){
+        document.getElementById("noqs2_op2").style.display = "block";
+        document.getElementById("noans2").innerHTML = "Ans: Norton's theorem is a simplified version of Thevenin's theorem "
+        document.getElementById("noans2").style.color = "red";
+
+    }
+    if(document.getElementById("noqs2_op3").checked == true){
+        document.getElementById("noqs2_op3").style.display = "block";
+        document.getElementById("noans2").innerHTML = "Ans: Norton's theorem is a simplified version of Thevenin's theorem  "
+        document.getElementById("noans2").style.color = "green";
+
+    }
+    if(document.getElementById("noqs2_op4").checked == true){
+        document.getElementById("noqs2_op4").style.display = "block";
+        document.getElementById("noans2").innerHTML = "Ans: Norton's theorem is a simplified version of Thevenin's theorem  "
+        document.getElementById("noans2").style.color = "red";
+
+    }
+    //////////ques-3//////////////////
+
+    if(document.getElementById("noqs3_op1").checked == true){
+        document.getElementById("noqs3_op1").style.display = "block";
+        document.getElementById("noans3").innerHTML = "Ans: Short-circuit current "
+        document.getElementById("noans3").style.color = "green";
+
+    }
+    if(document.getElementById("noqs3_op2").checked == true){
+        document.getElementById("noqs3_op2").style.display = "block";
+        document.getElementById("noans3").innerHTML = "Ans: Short-circuit current "
+        document.getElementById("noans3").style.color = "red";
+
+    }
+    if(document.getElementById("noqs3_op3").checked == true){
+        document.getElementById("noqs3_op3").style.display = "block";
+        document.getElementById("noans3").innerHTML = "Ans: Short-circuit current  "
+        document.getElementById("noans3").style.color = "red";
+
+    }
+    if(document.getElementById("noqs3_op4").checked == true){
+        document.getElementById("noqs3_op4").style.display = "block";
+        document.getElementById("noans3").innerHTML = "Ans: Short-circuit current "
+        document.getElementById("noans3").style.color = "red";
+
+    }
+    //////////ques-4//////////////////
+
+    if(document.getElementById("noqs4_op1").checked == true){
+        document.getElementById("noqs4_op1").style.display = "block";
+        document.getElementById("noans4").innerHTML = "Ans: The parallel equivalent resistance of the resistors in the circuit "
+        document.getElementById("noans4").style.color = "red";
+
+    }
+    if(document.getElementById("noqs4_op2").checked == true){
+        document.getElementById("noqs4_op2").style.display = "block";
+        document.getElementById("noans4").innerHTML = "Ans: The parallel equivalent resistance of the resistors in the circuit "
+        document.getElementById("noans4").style.color = "green";
+
+    }
+    if(document.getElementById("noqs4_op3").checked == true){
+        document.getElementById("noqs4_op3").style.display = "block";
+        document.getElementById("noans4").innerHTML = "Ans: The parallel equivalent resistance of the resistors in the circuit  "
+        document.getElementById("noans4").style.color = "red";
+
+    }
+    if(document.getElementById("noqs4_op4").checked == true){
+        document.getElementById("noqs4_op4").style.display = "block";
+        document.getElementById("noans4").innerHTML = "Ans: The parallel equivalent resistance of the resistors in the circuit "
+        document.getElementById("noans4").style.color = "red";
+
+    }
+    //////////ques-5//////////////////
+
+    if(document.getElementById("noqs5_op1").checked == true){
+        document.getElementById("noqs5_op1").style.display = "block";
+        document.getElementById("noans5").innerHTML = "Ans: When the circuit is complex and has many components  "
+        document.getElementById("noqs5_op1").style.color = "red";
+
+    }
+    if(document.getElementById("noqs5_op2").checked == true){
+        document.getElementById("noqs5_op2").style.display = "block";
+        document.getElementById("noans5").innerHTML = "Ans: When the circuit is complex and has many components "
+        document.getElementById("noans5").style.color = "green";
+
+    }
+    if(document.getElementById("noqs5_op3").checked == true){
+        document.getElementById("noqs5_op3").style.display = "block";
+        document.getElementById("noans5").innerHTML = "Ans: When the circuit is complex and has many components  "
+        document.getElementById("noans5").style.color = "red";
+
+    }
+    if(document.getElementById("noqs5_op4").checked == true){
+        document.getElementById("noqs5_op4").style.display = "block";
+        document.getElementById("noans5").innerHTML = "Ans: When the circuit is complex and has many components "
+        document.getElementById("noans5").style.color = "red";
+
+    }
+
+    //////////ques-6//////////////////
+
+    document.getElementById("noans6").style.display = "block";
+    document.getElementById("noans6").innerHTML = "Ans: Norton's theorem is a method for finding the equivalent resistance of a complex circuit. The purpose of Norton's theorem is to simplify the analysis of complex circuits by reducing them to a single equivalent resistance, making it easier to calculate the current and voltage in the circuit."
+    document.getElementById("noans6").style.color = "green";
+
+    //////////ques-7//////////////////
+
+    document.getElementById("noans7").style.display = "block";
+    document.getElementById("noans7").innerHTML = "Ans: Both Norton's theorem and Thevenin's theorem are methods for simplifying complex circuits, but they approach the problem from different perspectives. Norton's theorem finds the equivalent resistance of a circuit by reducing it to a single resistance, while Thevenin's theorem finds the equivalent voltage source and resistance of a circuit by reducing it to a single voltage and single resistance."
+    document.getElementById("noans7").style.color = "green";
+}
