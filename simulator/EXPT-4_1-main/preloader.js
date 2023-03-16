4cfe9e0f95a74e52c89163cf0e868ef429eef1ff
Suprita Panda <supritapanda17@gmail.com>
2023-03-17 01:48:57 +0530

    main

--- simulator/EXPT-4_1-main/preloader.js
+++
@@ -1,4 +0,0 @@
-const delayTime = 10000;
-    setTimeout(function() {
-    location.href = "EXPT-4.html";
-}, delayTime);
---
+++ simulator/EXPT-4_1/preloader.js
@@ -0,0 +1,4 @@
+const delayTime = 10000;
+    setTimeout(function() {
+    location.href = "EXPT-4.html";
+}, delayTime);
